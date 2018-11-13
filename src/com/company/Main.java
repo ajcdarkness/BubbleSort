f07be228ab99f8efa8c704a9ba9ddfb5d461d0fc
BT_1N3_33@BKTECH.LOCAL <BT_1N3_33@BKTECH.LOCAL>
2018-11-13 11:47:27 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public static void swap(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr.length; j++){}
+
+        }
+    }
+}
