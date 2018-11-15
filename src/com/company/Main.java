9983e151ad60e4efc427c64ef03700eb4ec852b3
Jacob Cedar <jcedar8640@bths.edu>
2018-11-15 11:21:03 -0500

    added mr folwells changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] nueces = {3 ,6 ,9, 1,2, 7, 8, 11, 23};
+	    bubbleSwap(nueces);
+	    for(int m = 0; m < nueces.length; m++){
+	        System.out.println(nueces[m]);
+        }
     }
     public static void swap(int[] arr, int i, int j){
         int temp = arr[i];
@@ -12,7 +16,7 @@ public class Main {
     }
     public static void bubbleSwap(int[] arr){
         for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr.length; j++) {
+            for(int j = 0; j < arr.length-1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     swap(arr, j, j + 1);
                 }
